e29674195a1ae759643813fb5d71710844dac24f
tomzy-ba <trbee100@gmail.com>
2024-03-09 14:02:41 +0000

    added models

---
+++ models/brand.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BrandSchema = new Schema({
+    name: { 
+        type: String,
+        required: true,
+        enum: ["Intel", "AMD", "Nvidia", "ASUS", "Corsair", "HyperX", "Other"],
+    },
+    location: { type: String, required: true, maxLength: 20},
+    CEO: {type: String, maxLength: 40},
+    foundedYear: {type: Number, max: 4},
+});
+
+BrandSchema.virtual("url").get(function () {
+    return `/brand/${this._id}`;
+});
+
+
+module.exports = mongoose.model("Brand", BrandSchema);
---
+++ models/part.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PartSchema = new Schema({
+    name: {type: String, required: true, maxLength: 50},
+    type: {
+        type: String,
+        required: true,
+        enum: ["CPU", "GPU", "RAM", "MotherBoard", "PSU"],
+    },
+    brand: { type: Schema.Types.ObjectId, ref: "Brand", required: true},
+});
+
+PartSchema.virtual("url").get(function () {
+    return `/part/${this._id}`;
+});
+
+
+module.exports = mongoose.model("Part", PartSchema);
