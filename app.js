37003a6910c0027069ce2a4595a5eca1c4bb356d
tomzy-ba <trbee100@gmail.com>
2024-03-09 16:51:20 +0000

    added navbar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
 require("dotenv").config();
 
 var indexRouter = require('./routes/index');
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
